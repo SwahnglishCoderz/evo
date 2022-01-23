36faa393cd3ab676990f6ae60cf84ea11fb7ff61
Jean Kyle <simplygenius78@gmail.com>
2022-01-23 14:26:01 +0300

    updated SQL file

--- evo.sql
+++ evo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 21, 2022 at 08:53 AM
+-- Generation Time: Jan 23, 2022 at 12:25 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.4.23
 
@@ -36,6 +36,46 @@ CREATE TABLE `access` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` text DEFAULT NULL,
+  `link` varchar(100) NOT NULL,
+  `title_tag` varchar(100) NOT NULL,
+  `icon` varchar(100) NOT NULL,
+  `position` int(11) NOT NULL,
+  `parent` int(11) DEFAULT NULL,
+  `section_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id`, `name`, `description`, `link`, `title_tag`, `icon`, `position`, `parent`, `section_id`) VALUES
+(1, 'group', 'All groups available, to which users are assigned', 'group/index', 'Groups', 'fas fa-users', 1, NULL, 2),
+(2, 'permissions', 'All available permissions, that can be assigned to specific groups.', '/permission/index', 'Permissions', 'fas fa-lock', 2, NULL, 2),
+(4, 'all users', 'This the users index page', '/users/index', 'Users', 'fas fa-users', 0, NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permission`
+--
+
+CREATE TABLE `permission` (
+  `id` int(11) NOT NULL,
+  `name` varchar(300) NOT NULL,
+  `section_id` int(11) NOT NULL,
+  `display_name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `remembered_logins`
 --
 
@@ -48,6 +88,26 @@ CREATE TABLE `remembered_logins` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `added_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `name`, `added_by`) VALUES
+(1, 'developer', 0),
+(2, 'subscriber', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `section`
 --
 
@@ -55,16 +115,18 @@ CREATE TABLE `section` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `description` varchar(500) NOT NULL DEFAULT 'Section description',
-  `position` int(11) NOT NULL
+  `position` int(11) NOT NULL,
+  `icon` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `section`
 --
 
-INSERT INTO `section` (`id`, `name`, `description`, `position`) VALUES
-(1, 'users', 'Section description', 1),
-(2, 'utility', 'Section description', 99);
+INSERT INTO `section` (`id`, `name`, `description`, `position`, `icon`) VALUES
+(1, 'users', 'Section description', 1, ''),
+(2, 'utility', 'Section description', 99, ''),
+(3, 'system', 'All the system configuration, including database setup, themes, emails, sms and so on. ', 98, '');
 
 -- --------------------------------------------------------
 
@@ -112,10 +174,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `password_hash`, `created_at`, `updated_at`, `activation_hash`, `status_id`, `is_active`, `password_reset_hash`, `password_reset_expires_at`) VALUES
 (20, 'Jean Kyle', 'john@example.com', '$2y$10$MOb5mkUXsJ8DsRBJQwlPluMKk47/4LRvSQIkDmS/EZPF1zxW91OAO', '2022-01-17 14:46:31', '0000-00-00 00:00:00', NULL, 2, 1, NULL, NULL),
-(21, 'Sarah Kishindo', 'sarah@example.com', '$2y$10$PwTaIneiKpKisoC.rl83L.uy4LlL2xh3DyQ64mIxJh6zPRNaZZ1gK', '2022-01-17 14:50:45', '0000-00-00 00:00:00', NULL, 2, 1, NULL, NULL),
-(32, 'Sige Pianist', 'sige@example.com', '$2y$10$Kocvp28vlK240z1sFG6oWODhQfDEigjm/rr/UwOI90W6V1BZRMgjS', '2022-01-19 09:52:25', '0000-00-00 00:00:00', NULL, 2, 1, NULL, NULL),
-(33, 'Ismail Thabit', 'isma@example.com', '$2y$10$YG/cVZinn0f2pj.9LrRO7u63jK4NJWXiMyB3V1lkq3WLuZ7OriDMG', '2022-01-19 17:32:54', '0000-00-00 00:00:00', 'd281e9ad192e4066188828ce1aa28151b62a11d9923613807551748593938acc', 2, 0, NULL, NULL),
-(34, 'usz', 'usz@example.com', '$2y$10$Vnjgu8njL6fGkDqYLiVOauMcVKDlMmDKGw/D4PwW075NCQVozqeSq', '2022-01-20 10:36:19', '0000-00-00 00:00:00', '5e362ebeada909b29d483bb22e8a6d4bf7957352aae6afe7a79dbf16dfefae49', 2, 0, NULL, NULL);
+(21, 'Sarah John', 'sarah@example.com', '$2y$10$PwTaIneiKpKisoC.rl83L.uy4LlL2xh3DyQ64mIxJh6zPRNaZZ1gK', '2022-01-17 14:50:45', '0000-00-00 00:00:00', NULL, 2, 1, NULL, NULL),
+(35, 'Sige Boy', 'sige@example.com', '$2y$10$fgYL6Q/VCprefMdOUpoci.ZzY1HoyiY92zeqVc1ILr1ufeM77bHIi', '2022-01-23 11:59:52', '0000-00-00 00:00:00', '6f083605a48b1db7d5c86c0d5d544ce18335bc16d8128a4fc0273c62141f1104', 2, 0, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -128,11 +188,31 @@ ALTER TABLE `access`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `section_id` (`section_id`);
+
+--
+-- Indexes for table `permission`
+--
+ALTER TABLE `permission`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `section_id` (`section_id`);
+
+--
 -- Indexes for table `remembered_logins`
 --
 ALTER TABLE `remembered_logins`
   ADD PRIMARY KEY (`token_hash`),
   ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `section`
@@ -165,10 +245,28 @@ ALTER TABLE `access`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `permission`
+--
+ALTER TABLE `permission`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `section`
 --
 ALTER TABLE `section`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `status`
@@ -180,11 +278,23 @@ ALTER TABLE `status`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `menu`
+--
+ALTER TABLE `menu`
+  ADD CONSTRAINT `section_id` FOREIGN KEY (`section_id`) REFERENCES `section` (`id`);
+
+--
+-- Constraints for table `permission`
+--
+ALTER TABLE `permission`
+  ADD CONSTRAINT `permission_ibfk_1` FOREIGN KEY (`section_id`) REFERENCES `section` (`id`);
 
 --
 -- Constraints for table `remembered_logins`
